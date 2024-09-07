5ee19431235d64ad336a571a46a47570bd4594d5
ImTrikk <patrickdionenofficial@gmail.com>
2024-09-07 19:19:33 +0800

    update: initial login UI added

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,49 @@
+import { useNavigate } from "react-router-dom";
+
 export const Login = () => {
+ const nav = useNavigate();
+
+ const navCreateAccount = () => {
+  nav("/signup");
+ };
+
  return (
   <>
-   <div>Login Page</div>
+   <div className="h-screen flex items-center justify-center">
+    <div className="bg-gray-100 w-[400px] h-auto p-5 rounded">
+     <div>Login Page</div>
+     <div className="flex flex-col gap-4 pt-5">
+      <div className="flex flex-col">
+       <label htmlFor="">Email</label>
+       <input
+        type="email"
+        placeholder="Enter your email"
+        className="px-4 h-10 rounded text-xs"
+       />
+      </div>
+      <div className="flex flex-col">
+       <label htmlFor="">Password</label>
+       <input
+        type="password"
+        placeholder="Enter your password"
+        className="px-4 h-10 rounded text-xs"
+       />
+       <p className="font-light text-xs pt-3">Forgot your password?</p>
+      </div>
+     </div>
+     <div className="mt-10 flex flex-col gap-3 items-center">
+      <button className="w-full bg-gray-500 h-10 text-white rounded">
+       Login
+      </button>
+      <p
+       onClick={navCreateAccount}
+       className="text-xs font-light cursor-pointer"
+      >
+       Dont have an account yet? Create account here
+      </p>
+     </div>
+    </div>
+   </div>
   </>
  );
 };
--- src/pages/landing page/Hero.jsx
+++ src/pages/landing page/Hero.jsx
@@ -3,20 +3,19 @@ import { Searchbar } from "../../components/Searchbar";
 export const Hero = () => {
  return (
   <>
-   <div className="flex items-center justify-center h-screen">
-    <div>
-     <div className="font-black text-5xl text-center leading-[40px]">
-      <h1>Foodie on the Go</h1>
-      <h1>With your Kodigo</h1>
-      <div className="pt-3">
-       <p className="text-[20px] font-light">
-        Explore, Save, Share, Interact, Review foods
-       </p>
-      </div>
+   <div className="flex flex-col items-center justify-center h-screen">
+    <div className="font-black text-5xl text-center leading-[50px]">
+     <h1>Foodie on the Go</h1>
+     <h1>With your Kodigo</h1>
+     <div className="pt-3">
+      <p className="text-[20px] font-light text-center">
+       Discover, Save, Share, and Savor: Explore Recipes, Connect with Cooks,
+       and Review Delicious Dishes.
+      </p>
      </div>
-     <div className="pt-5">
-      <Searchbar />
-     </div>
+    </div>
+    <div className="pt-5">
+     <Searchbar />
     </div>
    </div>
   </>
